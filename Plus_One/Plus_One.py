c445ff56ab8f8f423ae519ac687924d97ab02734
Jin Pei Ng <jin.pei.ng95@gmail.com>
2022-12-29 14:47:04 +0800

    added another solution for plus one

--- Plus_One/Plus_One.py
+++ Plus_One/Plus_One.py
@@ -6,3 +6,16 @@ class Solution:
             decimal = pow(10, length-1-i)
             result += digits[i] * decimal
         return [int(i) for i in str(result+1)]
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1, -1, -1):
+            digits[i] += 1
+            if (digits[i] >= 10):
+                digits[i] = 0
+            else:
+                return digits
+
+        digits.insert(0, 1)
+        return digits
